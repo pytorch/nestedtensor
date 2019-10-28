a7b5fc73e81cfc729cf5dd220fddc233b2748b47
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-10-28 12:15:02 -0700

    tensorwise tests

---
+++ test/test_nested_tensor_tensorwise.py
@@ -0,0 +1,72 @@
+import traceback
+import functools
+import pdb
+import sys
+from nestedtensor import torch
+import nestedtensor
+import unittest
+from unittest import TestCase
+import random
+
+import utils
+
+class TestTensorWise(TestCase):
+
+    def test_tensorwise(self):
+
+        @nestedtensor.tensorwise()
+        def simple_fn(t1, t2):
+            return t1 + 1 + t2 * 2
+
+        a = torch.tensor([1, 2])
+        b = torch.tensor([7, 8])
+        nt1 = torch.nested_tensor([a, b])
+        nt2 = torch.nested_tensor([b, a])
+
+    def test_tensorwise_nested_dim_2(self):
+
+        @nestedtensor.tensorwise()
+        def simple_fn(t1, t2):
+            return t1 + 1 + t2 * 2
+
+        a = torch.tensor([1, 2])
+        b = torch.tensor([7, 8])
+        nt1 = torch.nested_tensor([[a, b], [b]])
+        nt2 = torch.nested_tensor([[b, a], [a]])
+        nt3 = simple_fn(nt1, nt2)
+
+    def test_tensorwise_scalar(self):
+
+        @nestedtensor.tensorwise(unbind_args=[2])
+        def simple_fn_scalar(t1, t2, scalar):
+            return t1 + scalar + t2 * 2
+
+        a = torch.tensor([1, 2])
+        b = torch.tensor([7, 8])
+        nt1 = torch.nested_tensor([a, b])
+        nt2 = torch.nested_tensor([b, a])
+        c = simple_fn_scalar(a, b, 2.0)
+        nt3 = simple_fn_scalar(nt1, nt2, (2.0, 3.0))
+        self.assertTrue((c == nt3[0]).all())
+
+    def test_tensorwise_tensor_kwarg(self):
+
+        @nestedtensor.tensorwise(unbind_args=['out'])
+        def simple_fn(t1, t2, t3=None):
+            result = t1 * 2 + t2
+            if t3 is not None:
+                result = result + t3
+            return result
+
+        a = torch.tensor([1, 2])
+        b = torch.tensor([7, 8])
+        nt1 = torch.nested_tensor([a, b])
+        nt2 = torch.nested_tensor([b, a])
+        c1 = simple_fn(a, b, t3=torch.tensor((0.5, 0.7)))
+        c2 = simple_fn(b, a, t3=torch.tensor((0.5, 0.7)))
+        nt3 = simple_fn(nt1, nt2, t3=torch.tensor((0.5, 0.7)))
+        self.assertTrue((c2 == nt3[1]).all())
+        self.assertTrue((c1 == nt3[0]).all())
+
+if __name__ == "__main__":
+    unittest.main()
