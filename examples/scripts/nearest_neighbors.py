649ae52d15bae37d4432b049cd595f99ae7cdb21
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-11-21 18:39:46 -0800

    Checkpoint

--- examples/scripts/nearest_neighbors.py
+++ examples/scripts/nearest_neighbors.py
@@ -5,37 +5,60 @@ import pprint
 EMBED_DIM = 200
 
 SEED = 0
+
+
 def gen_tensor():
     globals()['SEED'] += 1
-    return torch.tensor([globals()['SEED']])
+    # return torch.tensor([globals()['SEED']])
+    return torch.rand(EMBED_DIM)
+
 
 def gen_clusters(num_clusters, size_range):
 
     def gen_cluster(num_entries):
         return [gen_tensor() for _ in range(num_entries)]
-    
+
     return [gen_cluster(random.randint(*size_range)) for _ in range(num_clusters)]
 
-clusters = gen_clusters(3, (2, 5))
 
-# Two keys for now
-keys = [gen_tensor(), gen_tensor()]
-# Simulating some overlap
-sub_clusters = [clusters[:3], clusters[2:]]
+def gen_algorithm(keys, sub_clusters):
+    def _algorithm():
+        results = []
+        for sub_cluster, key in zip(sub_clusters, keys):
+            sub_cluster_results = []
+            for cluster in sub_cluster:
+                sub_cluster_results.append(
+                    [torch.dot(key, entry).item() for entry in cluster])
+            results.append(sub_cluster_results)
+        return results
+    return _algorithm
 
-# Algorithm
+
+def print_results(results, keys, sub_clusters, print_details=False):
+    if print_details:
+        for i, sub_cluster in enumerate(sub_clusters):
+            print("\n\u001b[31msub cluster {} count {} total number of entries {}\u001b[0m".format(
+                i, len(sub_cluster), sum(map(len, sub_cluster))))
+            pprint.pprint(sub_cluster)
+        print("\nkeys")
+        pprint.pprint(keys)
+        print("")
+
+    for i, result in enumerate(results):
+        print(
+            "result scores for \u001b[31msub cluster {} and key {}\u001b[0m".format(i, i))
+        pprint.pprint(result)
 
 
-results = []
-for key in keys:
-    key_results = []
-    for sub_cluster in sub_clusters:
-        sub_cluster_results = []
-        for cluster in sub_cluster:
-            sub_cluster_results.append([torch.dot(key, entry) for entry in cluster])
-        key_results.append(sub_cluster_results)
-    results.append(key_results)
+if __name__ == "__main__":
+    clusters = gen_clusters(3, (2, 5))
 
-pprint.pprint(clusters)
-pprint.pprint(keys)
-pprint.pprint(results)
+    # Two keys for now
+    keys = [gen_tensor(), gen_tensor()]
+    # Simulating some overlap
+    sub_clusters = [clusters[:3], clusters[2:]]
+
+    # Get algorithm
+    gen_results = gen_algorithm(keys, sub_clusters)
+
+    print_results(gen_results(), keys, sub_clusters)
