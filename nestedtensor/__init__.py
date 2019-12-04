de5e3ba1cbffc87807c5ecc7b59bd628bb02f915
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-12-04 11:07:52 -0800

    Checkpoint

--- nestedtensor/__init__.py
+++ nestedtensor/__init__.py
@@ -1,8 +1,8 @@
 import torch
-if getattr(torch, "__IS_MONKEY_PATCHED_BY_NESTED_TENSOR", None) is None:
-    from .nested.monkey_patch import monkey_patch
-    torch = monkey_patch(torch)
-# Confirm that this function was only applied once
-assert torch.__IS_MONKEY_PATCHED_BY_NESTED_TENSOR == 1
+# if getattr(torch, "__IS_MONKEY_PATCHED_BY_NESTED_TENSOR", None) is None:
+#     from .nested.monkey_patch import monkey_patch
+#     torch = monkey_patch(torch)
+# # Confirm that this function was only applied once
+# assert torch.__IS_MONKEY_PATCHED_BY_NESTED_TENSOR == 1
 
 from .nested.utils import tensorwise
