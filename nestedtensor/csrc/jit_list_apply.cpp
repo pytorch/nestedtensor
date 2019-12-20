5347bdad6c72e1248069f4e4b122d7b83ea5ca95
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-12-19 20:29:08 -0800

    Checkpoint

--- nestedtensor/csrc/jit_list_apply.cpp
+++ nestedtensor/csrc/jit_list_apply.cpp
@@ -138,6 +138,121 @@ THP_ListNestedTensor jit_apply_function(
   return THP_ListNestedTensor(_ListNestedTensor(nested_node));
 }
 
+static bool try_match_schema(
+    const FunctionSchema* schema,
+    py::args py_args,
+    py::kwargs py_kwargs) {
+  // In the end it's only a match when this counter fully depleted the args.
+  size_t py_args_i = 0;
+  size_t used_kwargs = 0;
+  std::vector<bool> used_kwarg(py_kwargs.size(), false);
+  const std::vector<Argument>& schema_args = schema->arguments();
+  std::vector<ArgWrapper> parse_py_args;
+  // For each argument in the Schema, see if it can be matched up with the
+  // given python arguments to determine whether it's the right overload.
+  //
+  // First we resolve the python arguments to build list of candidate
+  // wrapped arguments. It's not enough to parse these arguments
+  // outside of a given Schema because of the type environment
+  // and conversions. It's possible to match a Python call
+  // signature to an overload with different types such as
+  // Scalar and Tensor etc. simply by requiring conversion.
+  bool fail = false;
+  for (size_t j = 0; j < schema_args.size(); j++) {
+    // TODO: Support for self as in tryMatchArgument?
+    Argument schema_arg = schema_args[j];
+    if (!schema_arg.kwarg_only() && py_args_i < py_args.size()) {
+      // TODO: Add support to allow conversions.
+      IValue ivalue = toTypeInferredIValue(py_args[py_args_i]);
+      parse_py_args.emplace_back(ArgWrapper(ivalue));
+      py_args_i++;
+    } else if (py_kwargs.contains(schema_arg.name().c_str())) {
+      // TODO: Check for no presence of duplicates in given schema
+      py::handle py_kwarg_object = py_kwargs[schema_arg.name().c_str()];
+      parse_py_args.emplace_back(
+          ArgWrapper(schema_arg.name(), toTypeInferredIValue(py_kwarg_object)));
+      used_kwargs++;
+    } else if (schema_arg.default_value()) {
+      parse_py_args.emplace_back(ArgWrapper(*schema_arg.default_value()));
+    } else {
+      // The given schema cannot find either a positional or keyword argument to
+      // match against for this given schema argument. There also is no default
+      // value specified for this schema argument. Therefore this schema cannot
+      // be the correct overload.
+      return false;
+    }
+  }
+  if (
+      // Check whether all positional arguments were matched by given Schema
+      (py_args.size() == py_args_i) &&
+      // Check if all kwargs were matched by given Schema
+      (used_kwargs == py_kwargs.size())) {
+    bool types_match = true;
+    TypeEnv type_env;
+    for (size_t j = 0; j < parse_py_args.size(); j++) {
+      std::cout << " ; parse_py_args[" << j
+                << "]: " << parse_py_args[j].ivalue().type()->str();
+      // Now that we found that the overall schema matches, we need to check
+      // whether the types match.
+      MatchTypeReturn match = matchTypeVariables(
+          schema_args[j].type(), parse_py_args[j].ivalue().type(), type_env);
+      types_match = types_match && match.success();
+    }
+    if (types_match) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// TODO: Write comparison operation based on a subset of Argument comparison
+c10::optional<Function*> resolve_builtin(
+    py::object obj,
+    py::args py_args,
+    py::kwargs py_kwargs) {
+  py::object builtin_name =
+      py::module::import("torch.jit").attr("_find_builtin")(obj);
+  auto name = c10::Symbol::fromQualString(py::str(builtin_name));
+
+  std::cout << "builtin_name: " << builtin_name << std::endl;
+  std::cout << "name: " << name << std::endl;
+
+  const auto& variants = getAllOperatorsFor(name);
+  const auto& builtin_functions = getAllBuiltinFunctionsFor(name);
+
+  // TODO: Move this into jit_tensorwise and add support for all 3 cases.
+  std::stringstream failure_messages;
+  std::vector<const FunctionSchema*> schemas;
+  for (const std::shared_ptr<Operator>& op : variants) {
+    schemas.push_back(&op->schema());
+  }
+  for (Function* method : builtin_functions) {
+    method->ensure_defined();
+    if (try_match_schema(&method->getSchema())) {
+      return method;
+    }
+  }
+
+  // Go through each Schema candidate based on the overloads
+  // The order here matters and is given by the way we construct schemas.
+  // This is a subset of matchSchemas within jit/script/schema_matching.cpp
+  // and only implements the argument matching based on features such as types.
+  // It could eventually live in the JIT as a subcomponent that can implement
+  // overload resolution generically and outside a graph context.
+  //
+  // In essence we spend most of our time resolving types (e.g. turn
+  // single floats into lists of floats, resolving concrete types) or dealing
+  // with the unordered nature of kwargs.
+  for (size_t i = 0; i < schemas.size(); i++) {
+    if (try_match_schema(schemas[i], py_args, py_kwargs)) {
+      std::cout << "schema[" << i << "]:\t" << *schemas[i];
+      std::cout << " - overload_name: " << schemas[i]->overload_name();
+      std::cout << "WIN" << std::endl;
+    }
+  }
+  return torch::ones({});
+}
+
 // TODO: This should support 3 types of functions
 // fn might be scripted (i.e. StrongFunctionPtr)
 // fn might be a builtin (need to resolve!)
@@ -168,110 +283,6 @@ py::cpp_function jit_tensorwise() {
       return THP_ListNestedTensor(_ListNestedTensor(result));
     });
   });
-}
-
-// TODO: Write comparison operation based on a subset of Argument comparison
-at::Tensor resolve_builtin(
-    py::object obj,
-    py::args py_args,
-    py::kwargs py_kwargs) {
-  py::object builtin_name =
-      py::module::import("torch.jit").attr("_find_builtin")(obj);
-  auto name = c10::Symbol::fromQualString(py::str(builtin_name));
-
-  std::cout << "builtin_name: " << builtin_name << std::endl;
-  std::cout << "name: " << name << std::endl;
-
-  const auto& variants = getAllOperatorsFor(name);
-  const auto& builtin_functions = getAllBuiltinFunctionsFor(name);
-
-  std::stringstream failure_messages;
-  std::vector<const FunctionSchema*> schemas;
-  for (const std::shared_ptr<Operator>& op : variants) {
-    schemas.push_back(&op->schema());
-  }
-  for (const auto method : builtin_functions) {
-    method->ensure_defined();
-    schemas.push_back(&method->getSchema());
-  }
-
-  // Go through each Schema candidate based on the overloads
-  // The order here matters and is given by the way we construct schemas.
-  // This is a subset of matchSchemas within jit/script/schema_matching.cpp
-  // and only implements the argument matching based on features such as types.
-  // It could eventually live in the JIT as a subcomponent that can implement
-  // overload resolution generically and outside a graph context.
-  //
-  // In essence we spend most of our time resolving types (e.g. turn
-  // single floats into lists of floats, resolving concrete types) or dealing
-  // with the unordered nature of kwargs.
-  for (size_t i = 0; i < schemas.size(); i++) {
-    const FunctionSchema* schema = schemas[i];
-    std::cout << "schema[" << i << "]:\t" << *schemas[i];
-    std::cout << " - overload_name: " << schemas[i]->overload_name();
-    // In the end it's only a match when this counter fully depleted the args.
-    size_t py_args_i = 0;
-    size_t used_kwargs = 0;
-    std::vector<bool> used_kwarg(py_kwargs.size(), false);
-    const std::vector<Argument>& schema_args = schema->arguments();
-    std::vector<ArgWrapper> parse_py_args;
-    // For each argument in the Schema, see if it can be matched up with the
-    // given python arguments to determine whether it's the right overload.
-    //
-    // First we resolve the python arguments to build list of candidate
-    // wrapped arguments. It's not enough to parse these arguments
-    // outside of a given Schema because of the type environment
-    // and conversions. It's possible to match a Python call
-    // signature to an overload with different types such as
-    // Scalar and Tensor etc. simply by requiring conversion.
-    bool fail = false;
-    for (size_t j = 0; j < schema_args.size(); j++) {
-      // TODO: Support for self as in tryMatchArgument?
-      Argument schema_arg = schema_args[j];
-      if (!schema_arg.kwarg_only() && py_args_i < py_args.size()) {
-        // TODO: Add support to allow conversions.
-        IValue ivalue = toTypeInferredIValue(py_args[py_args_i]);
-        parse_py_args.emplace_back(ArgWrapper(ivalue));
-        py_args_i++;
-      } else if (py_kwargs.contains(schema_arg.name().c_str())) {
-        // TODO: Check for no presence of duplicates in given schemas[i]
-        py::handle py_kwarg_object = py_kwargs[schema_arg.name().c_str()];
-        parse_py_args.emplace_back(ArgWrapper(
-            schema_arg.name(), toTypeInferredIValue(py_kwarg_object)));
-        used_kwargs++;
-      } else if (schema_arg.default_value()) {
-        parse_py_args.emplace_back(ArgWrapper(*schema_arg.default_value()));
-      } else {
-        // The given schema cannot find either a positional or keyword argument to match against
-        // for this given schema argument. There also is no default value specified for this 
-        // schema argument. Therefore this schema cannot be the correct overload.
-        fail = true;
-        break;
-      }
-    }
-    if (!fail && 
-        // Check whether all positional arguments were matched by given Schema
-        (py_args.size() == py_args_i) &&
-        // Check if all kwargs were matched by given Schema
-        (used_kwargs == py_kwargs.size())
-        ) {
-      bool types_match = true;
-      TypeEnv type_env;
-      for (size_t j = 0; j < parse_py_args.size(); j++) {
-        std::cout << " ; parse_py_args[" << j << "]: " << parse_py_args[j].ivalue().type()->str();
-        // Now that we found that the overall schema matches, we need to check whether the types match.
-        MatchTypeReturn match = matchTypeVariables(
-            schema_args[j].type(), parse_py_args[j].ivalue().type(), type_env);
-        types_match = types_match && match.success();
-      }
-      if (types_match) {
-        std::cout << "\t=== WIN" << std::endl;
-        continue;
-      }
-    }
-    std::cout << "\t=== FAIL" << std::endl;
-  }
-  return torch::ones({});
 }
 
 } // namespace nested_tensor
--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev201912204+c9b2bef'
-git_version = 'c9b2bef345cde60912cc9d78ef46b96c54e8fb3e'
+__version__ = '0.0.1.dev201912204+49d9887'
+git_version = '49d9887b58b1453e009c0256ba2dac7f1e19ce67'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
