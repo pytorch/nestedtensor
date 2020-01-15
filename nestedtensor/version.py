565e859612c61516d7de6df6bab2a34c913d9511
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-01-15 13:11:27 -0800

    Fix NestedTensor.size for empty NestedTensors

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202011421+1f8c400'
-git_version = '1f8c400ce056507c6cd002a528bf8f88c7c59ad2'
+__version__ = '0.0.1.dev202011520+832b078'
+git_version = '832b0780a5d3a865cf0c938c404b182e8b9247e8'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
