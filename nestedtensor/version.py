dfd465f43600c4b114b334fcbd75546c4bd010c3
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-01-20 12:27:35 -0800

    Checkpoint

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202012020+7a5cb4c'
-git_version = '7a5cb4c053f91db4b5665e1966df26896f5dd735'
+__version__ = '0.0.1.dev202012020+dfcac34'
+git_version = 'dfcac3458dd8d539b2ee0d9dfd6ac252207148d6'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
