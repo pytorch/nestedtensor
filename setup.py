a73ee2173077897efcf5a1c660fc5015a3e98aa3
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-02-07 13:57:32 -0800

    Checkpoint

--- setup.py
+++ setup.py
@@ -150,7 +150,7 @@ setuptools.setup(
     ],
     zip_safe=True,
     cmdclass={'clean': clean,
-              'build_ext': BuildExtension.with_options(use_ninja=True)},
+              'build_ext': BuildExtension.with_options(use_ninja=False)},
     install_requires=requirements,
     ext_modules=get_extensions()
 )
