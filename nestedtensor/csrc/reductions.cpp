6b9cf36219f56d565f5cdedb716e69331f110925
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-02-13 20:54:17 -0800

    Checkpoint

--- nestedtensor/csrc/reductions.cpp
+++ nestedtensor/csrc/reductions.cpp
@@ -1,18 +1,41 @@
 #include <reductions.h>
+#include <torch/torch.h>
 
 namespace torch {
 namespace nested_tensor {
 
 template <class F>
+auto reduction(F& fn) {
+  return [&fn](const THPNestedTensor& self) {
+    TensorNode result_node = map(
+        [&fn](Tensor data) {
+          Tensor result;
+          return fn(result, data, IntArrayRef({0}), false, c10::nullopt);
+        },
+        self.get_structure());
+    return THPNestedTensor(_ListNestedTensor(std::move(result_node)));
+  };
+}
+
+template <class F>
 void add_reduction(auto m, auto c, std::string name, F& at_out) {
+  m.def(name.c_str(), torch::nested_tensor::reduction(at_out));
+  // py::arg("input"),
+  // py::arg("out") = c10::nullopt);
+  c.def(name.c_str(), torch::nested_tensor::reduction(at_out));
 }
 
 void add_reductions_functions(
-    pybind11::module,
-    pybind11::class_<torch::nested_tensor::THPNestedTensor>) {
-
-    add_reduction(m, c, "sum", at::sum);
-
+    pybind11::module m,
+    pybind11::class_<torch::nested_tensor::THPNestedTensor> c) {
+  auto fn = [](Tensor& result,
+               const Tensor& self,
+               IntArrayRef dim,
+               bool keepdim,
+               optional<ScalarType> opt_dtype) {
+    return at::native::sum_out(result, self, dim, keepdim, opt_dtype);
+  };
+  add_reduction(m, c, "sum", fn);
 }
 
 } // namespace nested_tensor
--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20202136+f89a18a'
-git_version = 'f89a18aa9521e1f2cf6305a4f41775e979b89861'
+__version__ = '0.0.1.dev20202144+a78c30e'
+git_version = 'a78c30e0b29d34c94ed1f2453eb0665b8e0e7261'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
