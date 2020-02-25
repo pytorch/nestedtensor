6f527f58234f579385a43e5f9068a5d216cdd23d
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-02-24 19:07:29 -0800

    Checkpoint

--- nestedtensor/nested/nested.py
+++ nestedtensor/nested/nested.py
@@ -183,16 +183,17 @@ class NestedTensor(object):
         # TODO: Design choice: Return zip_longest or zip?
 
         dim = utils._wrap_dim(self, dim)
+        if dim == 0:
+            return tuple(t if torch.is_tensor(t) else NestedTensor(t)
+                         for t in self._impl.unbind(dim))
         if self.nested_dim() == 1:
             return tuple(t if torch.is_tensor(t) else NestedTensor(t)
                          for t in self._impl.unbind(dim))
         else:
-            if dim == 0:
-                return tuple(NestedTensor(t) for t in self._impl.unbind(0))
             tmp = tuple(t.unbind(dim - 1) for t in self.unbind())
-        tmp = list(list(filter(lambda x: x is not None, t))
-                   for t in itertools.zip_longest(*tmp))
-        return tuple(nestedtensor.nested_tensor(e) for e in tmp)
+            tmp = list(list(filter(lambda x: x is not None, t))
+                       for t in itertools.zip_longest(*tmp))
+            return tuple(nestedtensor.nested_tensor(e) for e in tmp)
 
     def to_tensor(self, dim=0):
         """
