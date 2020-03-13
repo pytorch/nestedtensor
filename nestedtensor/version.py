524f7b7ec3d40a90571d519aecc2886319072329
izdeby <iuriiz@fb.com>
2020-03-12 21:29:40 -0700

    batch_norm

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202031019+2791085'
-git_version = '27910851d4e9f07edef787a41eb5ff3bd22d1c07'
+__version__ = '0.0.1.dev202031220+0b18247'
+git_version = '0b182471c373874a5def9f1c54b1c74fa4e617d6'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
