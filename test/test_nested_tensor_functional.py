7c67e4ee2742bcb0651fe8b18d33875d0663044c
izdeby <iuriiz@fb.com>
2020-03-13 13:07:00 -0700

    covered all layers

--- test/test_nested_tensor_functional.py
+++ test/test_nested_tensor_functional.py
@@ -8,6 +8,7 @@ import unittest
 from utils import TestCase
 import random
 import utils
+import torchvision
 
 class TestFunctional(TestCase):
 
@@ -22,7 +23,7 @@ class TestFunctional(TestCase):
 
     def test_conv2d(self):
         tensor1 = torch.rand(3, 128, 128)
-        tensor2 = torch.rand(3, 128, 128) 
+        tensor2 = torch.rand(3, 300, 400) 
         list_of_tensors = [tensor1, tensor2]
 
         weight = torch.rand(3, 3, 7, 7)
@@ -59,6 +60,99 @@ class TestFunctional(TestCase):
 
         self.assertEqual(nestedtensor.nested_tensor(tensor_res), nt_res)
 
+    def test_max_pool2d(self):
+        inputs = [
+            torch.randn(3, 500, 600),
+            torch.randn(3, 128, 128)
+        ]
+
+        nt = nestedtensor.nested_tensor(inputs)
+        maxPool2d = torch.nn.MaxPool2d(3)
+
+        tensor_res = []
+        for i in range(2):
+            t_res = maxPool2d(inputs[i].unsqueeze(0).contiguous())
+            tensor_res.append(t_res.squeeze(0))
+
+        nt_res = maxPool2d(nt)
+
+        self.assertEqual(nestedtensor.nested_tensor(tensor_res), nt_res)
+
+    def test_max_relu(self):
+        inputs = [
+            torch.randn(3, 500, 600),
+            torch.randn(3, 128, 128)
+        ]
+        nt = nestedtensor.nested_tensor(inputs)
+
+        tensor_res = []
+        for i in range(2):
+            t_res = torch.nn.functional.relu(inputs[i].unsqueeze(0).contiguous())
+            tensor_res.append(t_res.squeeze(0))
+
+        nt_res = torch.nn.functional.relu(nt)
+
+        self.assertEqual(nestedtensor.nested_tensor(tensor_res), nt_res)
+
+    def test_cross_entropy(self):
+        inputs = [
+            torch.randn(3, 300, 300),
+            torch.randn(3, 400, 400)
+        ]
+
+        targets = [
+            torch.randint(1, (1, 300, 300), dtype=torch.int64),
+            torch.randint(1, (1, 400, 400), dtype=torch.int64)
+        ]
+
+        input_nt = nestedtensor.nested_tensor(inputs)
+        target_nt = nestedtensor.nested_tensor(targets)
+        nt_res = torch.nn.functional.cross_entropy(input_nt, target_nt)
+
+        tensor_res = []
+        for i in range(2):
+            t_res = torch.nn.functional.cross_entropy(inputs[i].unsqueeze(0).contiguous(), targets[i])
+            tensor_res.append(t_res.squeeze(0))
+
+        
+
+        self.assertEqual(nestedtensor.nested_tensor(tensor_res), nt_res)
+
+    def test_dropout(self):
+        inputs = [
+            torch.randn(3, 128, 128),
+            torch.randn(3, 300, 400)
+        ]
+
+        nt = nestedtensor.nested_tensor(inputs)
+
+        tensor_res = []
+        for i in range(2):
+            t_res = torch.nn.functional.dropout(inputs[i].unsqueeze(0).contiguous())
+            tensor_res.append(t_res.squeeze(0))
+
+        nt_res = torch.nn.functional.dropout(nt)
+
+        self.assertEqual(nestedtensor.nested_tensor(tensor_res).size(), nt_res.size())
+
+    def test_interpolate(self):
+        inputs = [
+            torch.randn(3, 128, 128),
+            torch.randn(3, 300, 400)
+        ]
+
+        sizes = [128, 300]
+        nt = nestedtensor.nested_tensor(inputs)
+
+        tensor_res = []
+        for i in range(2):
+            t_res = torch.nn.functional.interpolate(inputs[i].unsqueeze(0).contiguous(), inputs[i].unsqueeze(0).shape[-2])
+            tensor_res.append(t_res.squeeze(0))
+
+        nt_res = torch.nn.functional.interpolate(nt)
+        nt_res2 = torch.nn.functional.interpolate(nt, sizes)
+        self.assertEqual(nestedtensor.nested_tensor(tensor_res), nt_res)
+        self.assertEqual(nt_res, nt_res2)
 
 if __name__ == "__main__":
     unittest.main()
