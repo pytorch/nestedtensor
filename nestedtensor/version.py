5026e29c6b388c73357e6d7c136073cf3b46ee9c
iurii zdebskyi <47012416+izdeby@users.noreply.github.com>
2020-03-28 21:15:22 -0400

    functionals max_pool2d, batch_norm, relu, interpolate, cross_entropy, dropout (#76)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202032323+366c5fb'
-git_version = '366c5fb75ba84eb937fdbd400454351a19da02c6'
+__version__ = '0.0.1.dev202032322+c0c9486'
+git_version = 'c0c94868b6be06d687a33a5147d132244f2bc415'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
