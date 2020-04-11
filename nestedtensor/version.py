db914b065034a37b2dd28fa63eca60ce280d731e
Christian Puhrsch <cpuhrsch@fb.com>
2020-04-10 21:30:40 -0400

    Use int64_t instead of T for counter

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202041018+14819da'
-git_version = '14819da098e6943f3de76527340ea0357ae7efdd'
+__version__ = '0.0.1.dev20204111+5dc822c'
+git_version = '5dc822cc1f11669ef0b3c96c98923be365c644f2'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
