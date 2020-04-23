2b3eceea7910d70d20df87004fec88936bd58cd9
izdeby <iuriiz@fb.com>
2020-04-22 18:08:25 -0700

    upd

--- benchmarks/segmentation_layers.py
+++ benchmarks/segmentation_layers.py
@@ -11,40 +11,57 @@ import itertools
 class SegLayersBenchMark(object):
     def __init__(self, args):
         self.args = args
+        
+
+    def init_layers(self, channels):
         self.max_pool2d = torch.nn.MaxPool2d(
             kernel_size=(2, 2), stride=(2, 2), padding=(0, 0), dilation=(1, 1)
         )
-        self.conv2d = torch.nn.Conv2d(args.C, 3, kernel_size=(1, 1), bias=False)
-        self.batch_norm = torch.nn.BatchNorm2d(args.C, 1e-05, 0.1)
+        self.conv2d = torch.nn.Conv2d(channels, 3, kernel_size=(1, 1), bias=False)
+        self.batch_norm = torch.nn.BatchNorm2d(channels, 1e-05, 0.1)
         self.batch_norm.eval()
 
     def run(self):
-        for h_var, w_var, seed, layer in itertools.product(
-            self.args.HV, self.args.WV, self.args.seed, self.args.layers
+        for n, c, h, w, h_var, w_var, seed in itertools.product(
+                self.args.N, 
+                self.args.C,
+                self.args.H,
+                self.args.W,
+                self.args.HV, 
+                self.args.WV, 
+                self.args.seed
         ):
-            benchmark = getattr(self, layer)
-            self.inputs, self.targets = self.get_input(h_var, w_var, seed)
+            self.init_layers(c)
 
-            result = utils.benchmark_fn(benchmark(), warmup=self.args.warm)
-            result["H"] = self.args.H
-            result["W"] = self.args.W
-            result["h_var"] = h_var
-            result["w_var"] = w_var
-            result["seed"] = seed
-            result["avg_us"] = int(result["avg_us"])
-            result["std_us"] = int(result["std_us"])
-            print(",".join(str((str(n), result[n])) for n in sorted(result.keys())))
+            # generate inputs before iterating layers to have the same imput per layer
+            self.inputs, self.targets = self.get_input(n, c, h, w, h_var, w_var, seed)
+            
 
-    def get_input(self, h_var, w_var, seed):
+            for layer in self.args.layers:
+                benchmark = getattr(self, layer)
+
+                result = utils.benchmark_fn(benchmark(), warmup=self.args.warm)
+                result["N"] = n,
+                result["C"] = c,
+                result["H"] = h,
+                result["W"] = w,
+                result["h_var"] = h_var
+                result["w_var"] = w_var
+                result["seed"] = seed
+                result["avg_us"] = int(result["avg_us"])
+                result["std_us"] = int(result["std_us"])
+                print(",".join(str((str(n), result[n])) for n in sorted(result.keys())))
+
+    def get_input(self, n, c, h, w, h_var, w_var, seed):
         inputs = []
         targets = []
 
         torch.manual_seed(seed)
-        for i in range(self.args.N):
-            h = max(1, int(self.args.H + random.gauss(self.args.H, h_var)))
-            w = max(1, int(self.args.W + random.gauss(self.args.W, w_var)))
-            inputs.append(torch.randn(self.args.C, h, w))
-            targets.append(torch.randint(1, (h, w), dtype=torch.int64))
+        for i in range(n):
+            h_res = max(1, int(h + random.gauss(h, h_var)))
+            w_res = max(1, int(w + random.gauss(w, w_var)))
+            inputs.append(torch.randn(c, h_res, w_res))
+            targets.append(torch.randint(1, (h_res, w_res), dtype=torch.int64))
 
         return inputs, targets
 
@@ -238,10 +255,10 @@ class SegLayersBenchMark(object):
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("-L", dest="layers", type=str, nargs="+")
-    parser.add_argument("-N", dest="N", type=int)
-    parser.add_argument("-C", dest="C", type=int)
-    parser.add_argument("-H", dest="H", type=int)
-    parser.add_argument("-W", dest="W", type=int)
+    parser.add_argument("-N", dest="N", type=int, nargs="+")
+    parser.add_argument("-C", dest="C", type=int, nargs="+")
+    parser.add_argument("-H", dest="H", type=int, nargs="+")
+    parser.add_argument("-W", dest="W", type=int, nargs="+")
     parser.add_argument("-HV", dest="HV", type=int, nargs="+")
     parser.add_argument("-WV", dest="WV", type=int, nargs="+")
     parser.add_argument("-S", dest="seed", type=int, nargs="+")
@@ -257,3 +274,4 @@ def main(args):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
