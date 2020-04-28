8431ad8ce4f394fb7b38a6700132369ec433dadc
root <root@c7ce957d05bc>
2020-04-28 01:15:59 +0000

    Checkpoint

--- nestedtensor/nested/masking.py
+++ nestedtensor/nested/masking.py
@@ -116,8 +116,8 @@ def get_tensor_mask(nt, shape):
             if nt.dim() == 0:
                 return nt, torch.tensor(True)
 
-            tensor = pad_tensor_to_shape(nt, shape)
-            mask = pad_tensor_to_shape(nt.new_full(nt.size(), True), shape)
+            tensor = pad_tensor_to_shape(nt.detach().clone(), shape)
+            mask = pad_tensor_to_shape(nt.new_full(nt.size(), True, dtype=torch.bool), shape)
             return tensor, mask
 
         res_tensor = []
@@ -132,7 +132,9 @@ def get_tensor_mask(nt, shape):
 
         return torch.stack(res_tensor), torch.stack(res_mask)
 
-    return pad_nt(nt, shape)
+    t, m = pad_nt(nt, shape)
+    t.requires_grad_(nt.requires_grad)
+    return t, m
 
 
 # Return a tuple of a tensor and a mask that represent the given tensor list
