a05df886d6801fcd313d0e80caec7b52db10c4f4
cpuhrsch <cpuhrsch@fb.com>
2020-04-27 21:38:41 -0400

    Use torch.stack instead tolist for performance gains for to_tensor_mask (#141)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20204243+016f557'
-git_version = '016f557069e56aa6b564d3913f00add66fe125f8'
+__version__ = '0.0.1.dev202042723+9b2a76a'
+git_version = '9b2a76a3c2ef8e5aad5e32e88189c54adadf2436'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
