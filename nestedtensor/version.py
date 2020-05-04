0f8bc7434c0c9aedf62eef4a336ca95bddda029c
iurii zdebskyi <47012416+izdeby@users.noreply.github.com>
2020-05-04 13:29:54 -0400

    fixed build by adopting changes from pytorch (#147)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202042918+a3ec635'
-git_version = 'a3ec63582c98dcfd3efcecfd55cf77adc3b03111'
+__version__ = '0.0.1.dev20205417+6a6dd44'
+git_version = '6a6dd445d9931f1618269d8e11fb540e9b01ac45'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
