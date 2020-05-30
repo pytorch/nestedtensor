ec19fc6a251b96ee661e60093dd8a1041f535d79
Christian <cpuhrsch@gmail.com>
2020-05-29 21:42:12 -0400

    Eliminate some dead code

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20205300+ae253b3'
-git_version = 'ae253b325cf0a6d7fb1f0b4acc1aa15e6d4c5f44'
+__version__ = '0.0.1.dev20205301+57b95bd'
+git_version = '57b95bd5901283280613fd36aec8860295fcb3dc'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
