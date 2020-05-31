b48f664fa0f1b67d1319ebba783532e1b56e5aae
Christian Puhrsch <cpuhrsch@fb.com>
2020-05-31 19:15:32 -0400

    Default implementation for matmul

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202053121+54658b7'
-git_version = '54658b780793162b7aab6eedbc26b8bdf96bf157'
+__version__ = '0.0.1.dev202053123+f2b83ee'
+git_version = 'f2b83eeb1d4aeb36d41a704239b862d21ee53819'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
