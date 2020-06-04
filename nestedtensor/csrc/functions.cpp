d77a65fdbb9f816ba1f14882d2964c54e86f1b95
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-06-04 15:00:51 -0700

    upd conv2d

--- nestedtensor/csrc/functions.cpp
+++ nestedtensor/csrc/functions.cpp
@@ -3,6 +3,7 @@
 #include <torch/extension.h>
 #include <torch/library.h>
 #include <nestedtensor/csrc/nested_tensor.h>
+#include <chrono>
 
 using namespace torch::nn;
 namespace F = torch::nn::functional;
@@ -57,6 +58,69 @@ Tensor NestedTensor_conv2d(
     IntArrayRef padding,
     IntArrayRef dilation,
     int64_t groups) {
+      auto self_impl = get_nested_tensor_impl(input);
+      auto nt = self_impl->_data;
+      auto tensor_node = get_nested_tensor_structure(input);
+
+  if (padding[0] == 0 && padding[1] == 0 &&
+      stride[0] == 1 && stride[1] == 1) {
+
+      std::vector<at::Tensor> tensors;
+      std::vector<int> Hs;
+      std::vector<int> Ws;
+
+      for (auto tn : tensor_node.unbind()) {
+        Hs.push_back(tn.payload().size(1));
+        Ws.push_back(tn.payload().size(2));
+        tensors.push_back(tn.payload());
+      }
+
+      std::vector<at::Tensor> unfold_transpose;
+      for (auto t : tensors) {
+        unfold_transpose.push_back(
+          torch::nn::functional::unfold(t.unsqueeze(0), 
+                                        torch::nn::functional::UnfoldFuncOptions({weight.size(2), weight.size(3)}).padding(padding)
+                                                                                                                  .stride(stride)
+                                                                                                                  .dilation(dilation)
+                                        ).transpose(1,2)
+        );
+      }
+
+      std::vector<at::Tensor> matmul_trans_res;
+      for (auto t : unfold_transpose) {
+        matmul_trans_res.push_back(
+          t.matmul(weight.view({weight.size(0), -1}).t()).transpose(1,2)
+        );
+      }
+
+      std::cout << "check1" << std::endl;
+      std::vector<torch::nested_tensor::TensorNode> tensorNodes;
+      int i=0;
+      for (auto t : matmul_trans_res) {
+        int h = ((Hs[i] + 2 * padding[0] - dilation[0] * (weight.size(2) - 1) - 1) / stride[0]) + 1;
+        int w = ((Ws[i] + 2 * padding[1] - dilation[1] * (weight.size(3) - 1) - 1) / stride[1]) + 1;
+        std::cout << Hs[i] << " " << h << " " << w << std::endl;
+
+        tensorNodes.push_back(
+          torch::nested_tensor::TensorNode(
+            std::move(
+              torch::nn::functional::fold(t, 
+                                          torch::nn::functional::FoldFuncOptions(/*output size*/ {h, w}, 
+                                                                                 /*kernel size*/ {1, 1})
+                                                                                 .stride(stride)
+                                                                                 .padding(padding)
+                                                                                 .dilation(dilation)
+                                          )
+                      )
+          )
+        );
+        i++;
+      }
+
+      return at::detail::make_tensor<at::NestedTensorImpl>(
+        torch::nested_tensor::NestedTensor(torch::nested_tensor::TensorNode(std::move(tensorNodes))).to_nested_tensor(nt.nested_dim() - 1));
+  }
+
   return wrap_tensor_node(map(
       [&weight, &bias, &stride, &padding, &dilation, groups](at::Tensor t) {
         return at::convolution(
@@ -129,7 +193,11 @@ Tensor NestedTensor_max_pool2d(
     bool flag = false;
   }
   
-  if (!flag) {
+  if (flag) {
+    auto start = std::chrono::steady_clock::now();
+   
+   
+
     std::vector<at::Tensor> tensors;
     for (auto tn : tensor_node.unbind()) {
       tensors.push_back(tn.payload());
@@ -138,26 +206,43 @@ Tensor NestedTensor_max_pool2d(
     std::vector<at::Tensor> unfolded;
     for (auto t : tensors) { 
       unfolded.push_back(
-        torch::nn::functional::unfold(t.unsqueeze(0), torch::nn::functional::UnfoldFuncOptions(kernel_size).padding(padding).stride(stride).dilation(dilation))
+        torch::nn::functional::unfold(t.unsqueeze(0), 
+                                  torch::nn::functional::UnfoldFuncOptions(kernel_size).padding(padding).stride(stride).dilation(dilation))
       );
     }
 
-    auto cat = at::cat(unfolded, 2);
-    auto res = at::max_pool2d(
-                   cat,
-                   /*kernel size*/{9, 1},
-                   /*stride*/{9, 1},
-                   /*padding*/ 0,
-                   /*dilation*/ 1,
-                   /*ceil_mode*/ false);
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "chk1: " << elapsed_seconds.count() << "s\n";
 
+    auto start2 = std::chrono::steady_clock::now();
+    auto cat = at::cat(unfolded, 2);
+    auto maxpool_res = at::max_pool2d(
+      cat,
+                                      /*kernel size*/{9, 1},
+                                      /*stride*/{9, 1},
+                                      /*padding*/ 0,
+                                      /*dilation*/ 1,
+                                      /*ceil_mode*/ false);
+
+    auto end2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds2 = end2-start2;
+    std::cout << "chk2: " << elapsed_seconds2.count() << "s\n";
+    
+    auto start3 = std::chrono::steady_clock::now();
     std::vector<int64_t> split_sizes;
     for (auto t : unfolded) {
       split_sizes.push_back(t.size(2));
     }
 
-    auto splitted = at::split_with_sizes(res, IntArrayRef(split_sizes), 2);
+    auto splitted = at::split_with_sizes(maxpool_res, IntArrayRef(split_sizes), 2);
 
+    auto end3 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds3 = end3-start3;
+    std::cout << "chk3: " << elapsed_seconds3.count() << "s\n";
+
+
+    auto start4 = std::chrono::steady_clock::now();
     std::vector<torch::nested_tensor::TensorNode> tensorNodes;
     for (int i = 0; i < tensors.size(); i++) {
       std::vector<int64_t> sizes;
@@ -173,6 +258,10 @@ Tensor NestedTensor_max_pool2d(
       torch::nested_tensor::TensorNode node = torch::nested_tensor::TensorNode(std::move(folded.squeeze(0)));
       tensorNodes.push_back(node);
     }
+
+    auto end4 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds4 = end4-start4;
+    std::cout << "chk4: " << elapsed_seconds4.count() << "s\n";
 
     return at::detail::make_tensor<at::NestedTensorImpl>(
       torch::nested_tensor::NestedTensor(torch::nested_tensor::TensorNode(std::move(tensorNodes))).to_nested_tensor(nt.nested_dim() - 1));
