ddc08894939441dac802dfbd049cda8bab54422e
Christian Puhrsch <cpuhrsch@fb.com>
2020-06-05 23:20:02 -0400

    Checkpoint

--- nestedtensor/csrc/nested_tensor_impl.cpp
+++ nestedtensor/csrc/nested_tensor_impl.cpp
@@ -219,28 +219,6 @@ Tensor NestedTensor_any(const Tensor& self) {
   return gathered.any();
 }
 
-Tensor NestedTensor_eq(const Tensor& self, const Tensor& other) {
-  auto self_impl = get_nested_tensor_impl(self);
-  auto other_impl = get_nested_tensor_impl(other);
-  return at::detail::make_tensor<NestedTensorImpl>(
-  map([](const Tensor a, const Tensor b) {
-      return at::eq(a, b);
-      }, 
-      self_impl->_data.get_structure(),
-      other_impl->_data.get_structure()));
-}
-
-Tensor NestedTensor_ne(const Tensor& self, const Tensor& other) {
-  auto self_impl = get_nested_tensor_impl(self);
-  auto other_impl = get_nested_tensor_impl(other);
-  return at::detail::make_tensor<NestedTensorImpl>(
-  map([](const Tensor a, const Tensor b) {
-      return at::ne(a, b);
-      }, 
-      self_impl->_data.get_structure(),
-      other_impl->_data.get_structure()));
-}
-
 Tensor NestedTensor_clone(const Tensor& src, c10::optional<c10::MemoryFormat> optional_memory_format) {
   auto self_impl = get_nested_tensor_impl(src);
   return at::detail::make_tensor<NestedTensorImpl>(
--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev2020663+474b0cc'
-git_version = '474b0cc2b8b6256ffbe553fecd463106cc2625bc'
+__version__ = '0.0.1.dev2020663+3ec50e8'
+git_version = '3ec50e8b9abd9d98e5b8ad972ff9bcc1325acf15'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
