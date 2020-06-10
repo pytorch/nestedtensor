5574fbc90bec980a3a4025a370ffc8c5bafc246e
iurii zdebskyi <47012416+izdeby@users.noreply.github.com>
2020-06-10 12:46:10 -0400

    fix interpolate (#171)
    Co-authored-by: Iurii Zdebskyi <iuriiz@devfair004.maas>

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20206105+313dbce'
-git_version = '313dbcec5900dfbf40f0c849acd9e7608cdc6ee2'
+__version__ = '0.0.1.dev202061015+c1f551b'
+git_version = 'c1f551bf05ad1bb7896433eefee8f8863ae4de99'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
