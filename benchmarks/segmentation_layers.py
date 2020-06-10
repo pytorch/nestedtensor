8afa4aa4c01289b77113ba53b8f36f6f65edab98
iurii zdebskyi <47012416+izdeby@users.noreply.github.com>
2020-06-10 13:52:51 -0400

    Fix cuda mode in segmentation benchmarks (#172)
    * fixed cuda
    
    * upd
    
    * upd
    
    * Update segmentation_layers.py

--- benchmarks/segmentation_layers.py
+++ benchmarks/segmentation_layers.py
@@ -280,7 +280,7 @@ class SegLayersBenchMark(object):
                 ),
             )
         try:
-            if cuda:
+            if cuda and layer is not None:
                 layer.cuda()
             return Benchmarks[name](self) if layer is None else Benchmarks[name](self, layer)
         except KeyError:
@@ -310,7 +310,6 @@ class SegLayersBenchMark(object):
             self.inputs, self.targets = self.get_input(cuda, n, c, h, w, h_var, w_var, seed)
 
             benchmarks = [(layer, self.get_benchmark(c, layer, cuda)) for layer in self.args.layers]
-
             for layer, benchmark in benchmarks:
                 result = utils.benchmark_fn(benchmark, run_time=self.args.run_time, warmup=self.args.warmup)
                 result["#"] = str(i) + "/" + str(len(benchmarks) * len(params))
