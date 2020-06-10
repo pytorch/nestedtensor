58e4ad0f1a3a5974ddc88b1d817bd059dc8e75fe
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-06-09 19:27:40 -0700

    Checkpoint

--- nestedtensor/csrc/functions.cpp
+++ nestedtensor/csrc/functions.cpp
@@ -62,31 +62,25 @@ Tensor NestedTensor_max_pool2d(
       auto buffer = nt.get_buffer();
       auto tensor = torch::reshape(buffer.value(), self_impl->sizes());
 
-      auto res = at::max_pool2d(tensor,
-                                kernel_size,
-                                stride,
-                                padding,
-                                dilation,
-                                ceil_mode);
+      auto res = at::max_pool2d(
+          tensor, kernel_size, stride, padding, dilation, ceil_mode);
 
       return at::detail::make_tensor<NestedTensorImpl>(
-        torch::nested_tensor::NestedTensor(std::move(res)).to_nested_tensor(nt.nested_dim() - 1));
+          torch::nested_tensor::NestedTensor(std::move(res))
+              .to_nested_tensor(nt.nested_dim() - 1));
     }
 
     std::vector<at::Tensor> tensors;
     for (auto tn : tensor_node.unbind()) {
       tensors.push_back(tn.payload());
     }
-    
-    auto res = at::max_pool2d(at::stack(tensors),
-                              kernel_size,
-                              stride,
-                              padding,
-                              dilation,
-                              ceil_mode);
+
+    auto res = at::max_pool2d(
+        at::stack(tensors), kernel_size, stride, padding, dilation, ceil_mode);
 
     return at::detail::make_tensor<NestedTensorImpl>(
-      torch::nested_tensor::NestedTensor(std::move(res)).to_nested_tensor(nt.nested_dim() - 1));
+        torch::nested_tensor::NestedTensor(std::move(res))
+            .to_nested_tensor(nt.nested_dim() - 1));
   }
 
   return wrap_tensor_node(map(
@@ -104,9 +98,15 @@ Tensor NestedTensor_max_pool2d(
 }
 
 Tensor NestedTensor_batch_norm(
-    const Tensor& input, const Tensor& weight /* optional */, const Tensor& bias /* optional */,
-    const Tensor& running_mean /* optional */, const Tensor& running_var /* optional */,
-    bool training, double momentum, double eps, bool cudnn_enabled) {
+    const Tensor& input,
+    const Tensor& weight /* optional */,
+    const Tensor& bias /* optional */,
+    const Tensor& running_mean /* optional */,
+    const Tensor& running_var /* optional */,
+    bool training,
+    double momentum,
+    double eps,
+    bool cudnn_enabled) {
   return wrap_tensor_node(map(
       [&](at::Tensor t) {
         return at::batch_norm(
@@ -124,7 +124,7 @@ Tensor NestedTensor_batch_norm(
       get_nested_tensor_structure(input)));
 }
 
-Tensor NestedTensor_sum(const Tensor &self, c10::optional<ScalarType> dtype) {
+Tensor NestedTensor_sum(const Tensor& self, c10::optional<ScalarType> dtype) {
   auto tensors = flatten(
       map([&dtype](at::Tensor tensor) { return at::sum(tensor, dtype); },
           get_nested_tensor_structure(self)));
@@ -140,11 +140,14 @@ Tensor NestedTensor_sum(const Tensor &self, c10::optional<ScalarType> dtype) {
 
 Tensor NestedTensor_reshape(const Tensor& self, IntArrayRef size) {
   auto self_data = get_nested_tensor(self);
-  TORCH_CHECK(int64_t(size.size()) > self_data.nested_dim(), "Reshape cannot include nested dimensions.");
+  TORCH_CHECK(
+      int64_t(size.size()) > self_data.nested_dim(),
+      "Reshape cannot include nested dimensions.");
   for (int64_t i = 0; i < self_data.nested_dim(); i++) {
     if (size[0] >= 0) {
-      throw std::runtime_error("Cannot reshape explicitly along nested dimension "
-          + std::to_string(size[i]));
+      throw std::runtime_error(
+          "Cannot reshape explicitly along nested dimension " +
+          std::to_string(size[i]));
     }
   }
   int64_t nested_dim = self_data.nested_dim();
@@ -152,14 +155,14 @@ Tensor NestedTensor_reshape(const Tensor& self, IntArrayRef size) {
   for (int64_t i = nested_dim; i < size.size(); i++) {
     target_shape.push_back(size[i]);
   }
-  return wrap_tensor_node(
-      map([target_shape](const at::Tensor t) { 
+  return wrap_tensor_node(map(
+      [target_shape](const at::Tensor t) {
         return at::reshape(t, IntArrayRef(target_shape));
       },
       get_nested_tensor_structure(self)));
 }
 
-Tensor NestedTensor_transpose(const Tensor & self, int64_t dim0, int64_t dim1) {
+Tensor NestedTensor_transpose(const Tensor& self, int64_t dim0, int64_t dim1) {
   auto self_data = get_nested_tensor(self);
   auto ndims = self_data.dim();
   dim0 = maybe_wrap_dim(dim0, ndims);
@@ -168,25 +171,29 @@ Tensor NestedTensor_transpose(const Tensor & self, int64_t dim0, int64_t dim1) {
     return self;
   }
   int64_t nested_dim = self_data.nested_dim();
-  TORCH_CHECK(dim0 >= nested_dim
-           && dim1 >= nested_dim, "Transposition of nested dimensions is not implemented yet.");
-  return wrap_tensor_node(
-      map([dim0, dim1, nested_dim](const at::Tensor t) { 
+  TORCH_CHECK(
+      dim0 >= nested_dim && dim1 >= nested_dim,
+      "Transposition of nested dimensions is not implemented yet.");
+  return wrap_tensor_node(map(
+      [dim0, dim1, nested_dim](const at::Tensor t) {
         return at::transpose(t, dim0 - nested_dim, dim1 - nested_dim);
-        },
+      },
       get_nested_tensor_structure(self)));
-
 }
 
-Tensor NestedTensor_softmax(const Tensor& input, const int64_t dim_, c10::optional<ScalarType> dtype) {
+Tensor NestedTensor_softmax(
+    const Tensor& input,
+    const int64_t dim_,
+    c10::optional<ScalarType> dtype) {
   int64_t dim = maybe_wrap_dim(dim_, input.dim());
   auto input_data = get_nested_tensor(input);
   int64_t nested_dim = input_data.nested_dim();
-  TORCH_CHECK(dim >= nested_dim, "Cannot apply softmax across nested dimensions");
-  return wrap_tensor_node(
-      map([dim, nested_dim, dtype](const at::Tensor t) { 
+  TORCH_CHECK(
+      dim >= nested_dim, "Cannot apply softmax across nested dimensions");
+  return wrap_tensor_node(map(
+      [dim, nested_dim, dtype](const at::Tensor t) {
         return at::softmax(t, dim - nested_dim, dtype);
-        },
+      },
       get_nested_tensor_structure(input)));
 }
 
@@ -197,10 +204,10 @@ Tensor NestedTensor_layer_norm(
     const Tensor& bias /* optional */,
     double eps,
     bool /* cudnn_enable, deprecated */) {
-  return wrap_tensor_node(
-      map([normalized_shape, &weight, &bias, eps](const at::Tensor t) { 
+  return wrap_tensor_node(map(
+      [normalized_shape, &weight, &bias, eps](const at::Tensor t) {
         return at::layer_norm(t, normalized_shape, weight, bias, eps, true);
-        },
+      },
       get_nested_tensor_structure(input)));
 }
 
@@ -227,8 +234,8 @@ Tensor NestedTensor_all(const Tensor& self) {
     result.fill_(1);
     return result;
   }
-  auto map_all = flatten(map(
-        [](at::Tensor tensor) { return tensor.all(); },
+  auto map_all = flatten(
+      map([](at::Tensor tensor) { return tensor.all(); },
           self_impl.get_structure()));
   at::Tensor gathered = at::empty(
       {static_cast<int64_t>(map_all.size())}, at::kBool); //, self.options());
@@ -247,8 +254,8 @@ Tensor NestedTensor_any(const Tensor& self) {
     result.fill_(1);
     return result;
   }
-  auto map_any = flatten(map(
-        [](at::Tensor tensor) { return tensor.any(); },
+  auto map_any = flatten(
+      map([](at::Tensor tensor) { return tensor.any(); },
           self_impl.get_structure()));
   at::Tensor gathered = at::empty(
       {static_cast<int64_t>(map_any.size())}, at::kBool); //, self.options());
@@ -258,21 +265,22 @@ Tensor NestedTensor_any(const Tensor& self) {
   return gathered.any();
 }
 
-Tensor NestedTensor__log_softmax(const Tensor& input_, const int64_t dim_, const bool half_to_float) {
+Tensor NestedTensor__log_softmax(
+    const Tensor& input_,
+    const int64_t dim_,
+    const bool half_to_float) {
   auto self_impl = get_nested_tensor_impl(input_);
   return at::detail::make_tensor<NestedTensorImpl>(
-      map([&](Tensor a) {
-          return at::_log_softmax(a, dim_, half_to_float);
-          }, 
+      map([&](Tensor a) { return at::_log_softmax(a, dim_, half_to_float); },
           self_impl->_data.get_structure()));
 }
 
 Tensor NestedTensor_matmul(const Tensor& self, const Tensor& other) {
   if (is_nested_tensor_impl(other)) {
-    return wrap_tensor_node(
-        map([](Tensor tensor, Tensor other) { return at::matmul(tensor, other); },
-            get_nested_tensor_structure(self),
-            get_nested_tensor_structure(other)));
+    return wrap_tensor_node(map(
+        [](Tensor tensor, Tensor other) { return at::matmul(tensor, other); },
+        get_nested_tensor_structure(self),
+        get_nested_tensor_structure(other)));
   }
   return wrap_tensor_node(
       map([&other](Tensor tensor) { return at::matmul(tensor, other); },
@@ -293,6 +301,12 @@ Tensor& NestedTensor_matmul_out(
   return result;
 }
 
+Tensor NestedTensor_pin_memory(const Tensor& self) {
+  return wrap_tensor_node(
+      map([](Tensor tensor) { return at::native::pin_memory(tensor); },
+          get_nested_tensor_structure(self)));
+}
+
 TORCH_LIBRARY_IMPL(aten, PrivateUse1_PreAutograd, m) {
   m.impl_UNBOXED("conv2d", NestedTensor_conv2d);
   m.impl_UNBOXED("batch_norm", NestedTensor_batch_norm);
@@ -308,8 +322,8 @@ TORCH_LIBRARY_IMPL(aten, PrivateUse1_PreAutograd, m) {
   m.impl_UNBOXED("transpose.int", NestedTensor_transpose);
   m.impl_UNBOXED("softmax.int", NestedTensor_softmax);
   m.impl_UNBOXED("layer_norm", NestedTensor_layer_norm);
-
   m.impl_UNBOXED("matmul", NestedTensor_matmul);
   m.impl_UNBOXED("matmul.out", NestedTensor_matmul_out);
+  m.impl_UNBOXED("pin_memory", NestedTensor_pin_memory);
 }
-}
+} // namespace at
--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20206919+18ccf04'
-git_version = '18ccf049d38d3ae112505e284d0c59d89d479250'
+__version__ = '0.0.1.dev20206102+fbdcb6e'
+git_version = 'fbdcb6e057f57f1654f0c68b45d2ee57f592c0bd'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
