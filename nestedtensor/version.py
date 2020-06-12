0b502619c88394a8605cb95dde41f87a8fadf1fd
cpuhrsch <cpuhrsch@fb.com>
2020-06-11 23:47:36 -0400

    Merge nested_tensor.h/cpp and nested_tensor_impl.h/cpp (#173)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202061015+c1f551b'
-git_version = 'c1f551bf05ad1bb7896433eefee8f8863ae4de99'
+__version__ = '0.0.1.dev20206123+a09f6fa'
+git_version = 'a09f6fa69ed660684bbfd06aa4eae08f2c61a6b0'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
