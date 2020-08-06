41574b58e752f04c47ca8dedb05a2d2cc4da9fe4
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-08-06 10:11:09 -0700

    Implement opt args for new aten function signatures

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202072320+1afe9bf'
-git_version = '1afe9bfcb860fad00e53be1b1ddf3bff9aaf05e8'
+__version__ = '0.0.1.dev20208617+4561ec9'
+git_version = '4561ec9d887654d86ba94232b32425b40cf774bf'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
