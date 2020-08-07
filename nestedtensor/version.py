544489528173f7d36de87b1b19afc98c357007b4
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-08-07 07:31:40 -0700

    Fix memory leak due to lack of Py_DECREF after indexing

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20208622+d1ab507'
-git_version = 'd1ab507267fd0eb57f32454431ce6d8e7965026a'
+__version__ = '0.0.1.dev20208714+3747381'
+git_version = '37473811913acec8ac6a4693dc41311a2e65bce8'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
