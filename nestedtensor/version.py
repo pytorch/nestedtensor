2e11f3877d889e6a72f2d43098c07ff8cd36ecdc
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-08-14 14:37:12 -0700

    Fix getitem bug

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202081416+bafd896'
-git_version = 'bafd89620a11c3fdf7d561d415d1303786f5f2e4'
+__version__ = '0.0.1.dev202081421+9fbd9b3'
+git_version = '9fbd9b39ca120fdb03268c130938b96c63d222fd'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
