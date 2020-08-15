7fd039d37fea7525d4fa495bfce5f1d787e62a0b
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-08-14 21:12:35 -0700

    Remove unnecessary wrap_tensor_node.

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20208153+e549cfe'
-git_version = 'e549cfe7d70f5146abee09adc6c3360b2f851b2c'
+__version__ = '0.0.1.dev20208154+5ef0bd5'
+git_version = '5ef0bd51ce1f79378d6b5a65b2fa9e4441f4f8a9'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
