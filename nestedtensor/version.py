81f0522521d33ca1ee512d9ad2a042697c78e8a1
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-08-27 17:54:52 -0700

    Checkpoint

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20208280+2639ffb'
-git_version = '2639ffb4728c1cd0079d0c0d0016ad51e40fad76'
+__version__ = '0.0.1.dev20208280+c1fd6d2'
+git_version = 'c1fd6d271af9cf58929a2ab3b2a93d75ee8f88d4'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
--- test/test_nested_tensor_autograd_functional.py
+++ test/test_nested_tensor_autograd_functional.py
@@ -288,22 +288,86 @@ class TestAutogradFunctional(TestCase):
         result = ntnt([t])
         nt = ntnt([t.reshape(1, 2, 3)])
         self.assertEqual(nt.squeeze(1), result)
-        self.assertRaisesRegex(RuntimeError, "Cannot squeeze first dimension.", lambda: nt.squeeze(0))
-        self.assertRaisesRegex(RuntimeError, "Given dimension is either undefined or not a singleton.", lambda: nt.squeeze(2))
-        self.assertRaisesRegex(RuntimeError, "Given dimension is either undefined or not a singleton.", lambda: nt.squeeze(3))
+        self.assertRaisesRegex(
+            RuntimeError, "Cannot squeeze first dimension.", lambda: nt.squeeze(0))
+        self.assertRaisesRegex(
+            RuntimeError, "Given dimension is either undefined or not a singleton.", lambda: nt.squeeze(2))
+        self.assertRaisesRegex(
+            RuntimeError, "Given dimension is either undefined or not a singleton.", lambda: nt.squeeze(3))
         self.assertRaises(IndexError, lambda: nt.squeeze(4))
         a = nt.squeeze(1)
         a.sum().backward()
-        self.assertEqual(nt.grad, ntnt_nograd([t.reshape(1, 2, 3).mul(0).add(1)]))
+        self.assertEqual(nt.grad, ntnt_nograd(
+            [t.reshape(1, 2, 3).mul(0).add(1)]))
 
         nt = ntnt([[t.reshape(1, 2, 1, 3)]])
-        self.assertRaisesRegex(RuntimeError, "Cannot squeeze nested dimension.", lambda: nt.squeeze(1))
+        self.assertRaisesRegex(
+            RuntimeError, "Cannot squeeze nested dimension.", lambda: nt.squeeze(1))
         # self.assertEqual(nt.squeeze(1), ntnt(
         #     [t.reshape(1, 2, 1, 3)]))
         self.assertEqual(nt.squeeze(
             2), ntnt([[t.reshape(2, 1, 3)]]))
         self.assertEqual(nt.squeeze(
             4), ntnt([[t.reshape(1, 2, 3)]]))
+
+    def test_fzbn2d(self):
+        class FrozenBatchNorm2d(torch.nn.Module):
+            """
+            BatchNorm2d where the batch statistics and the affine parameters are fixed.
+            Copy-paste from torchvision.misc.ops with added eps before rqsrt,
+            without which any other models than torchvision.models.resnet[18,34,50,101]
+            produce nans.
+            """
+
+            def __init__(self, n):
+                super(FrozenBatchNorm2d, self).__init__()
+                self.register_buffer("weight", torch.ones(n))
+                self.register_buffer("bias", torch.zeros(n))
+                self.register_buffer("running_mean", torch.zeros(n))
+                self.register_buffer("running_var", torch.ones(n))
+
+            def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                                      missing_keys, unexpected_keys, error_msgs):
+                num_batches_tracked_key = prefix + 'num_batches_tracked'
+                if num_batches_tracked_key in state_dict:
+                    del state_dict[num_batches_tracked_key]
+
+                super(FrozenBatchNorm2d, self)._load_from_state_dict(
+                    state_dict, prefix, local_metadata, strict,
+                    missing_keys, unexpected_keys, error_msgs)
+
+            def forward(self, x):
+                # move reshapes to the beginning
+                # to make it fuser-friendly
+                w = self.weight.reshape(1, -1, 1, 1)
+                b = self.bias.reshape(1, -1, 1, 1)
+                rv = self.running_var.reshape(1, -1, 1, 1)
+                rm = self.running_mean.reshape(1, -1, 1, 1)
+                eps = 1e-5
+                scale = w * (rv + eps).rsqrt()
+                bias = b - rm * scale
+                return (x * scale + bias).squeeze(1)
+
+        b0 = FrozenBatchNorm2d(64)  # .cuda()
+        import random
+        random.seed(1010)
+        torch.manual_seed(1310)
+        RAND_INTS = [random.randint(100, 300) for _ in range(20)]
+        tensors = [torch.rand(64, i, 256, requires_grad=True) for i in RAND_INTS]
+        nested_tensor = nestedtensor.nested_tensor(tensors,
+                                                   device=torch.device('cpu'), dtype=torch.float, requires_grad=True)
+        s0 = b0(nested_tensor).sum()
+        s0.backward()
+
+        b1 = FrozenBatchNorm2d(64)  # .cuda()
+        s1 = 0
+        for t in tensors:
+            s1 += b1(t).sum()
+        s1.backward()
+        self.assertEqual(s0, s1)
+
+        self.assertEqual(len((list(b0.named_parameters()))), 0)
+        self.assertEqual(len((list(b1.named_parameters()))), 0)
 
 
 if __name__ == "__main__":
