f75ebd44dc72675a80e7c7ee72c99c30203387f2
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-08-30 22:40:32 -0700

    Checkpoint

--- benchmarks/mha.py
+++ benchmarks/mha.py
@@ -9,29 +9,30 @@ import random
 random.seed(1010)
 RAND_INTS = [random.randint(10, 30) for _ in range(2000)]
 RAND_INTS = [random.randint(100, 300) for _ in range(20)]
+DEVICE = torch.device('cpu')
 
 # (26, None, 256) (26, None, 256) (26, None, 256) torch.Size([256, 256]) torch.Size([256])
-MODEL0 = torch.nn.MultiheadAttention(256, 8, dropout=0.1).cuda()
-MODEL1 = nestedtensor.nn.MultiheadAttention(256, 8, dropout=0.1).cuda()
+MODEL0 = torch.nn.MultiheadAttention(256, 8, dropout=0.1).to(DEVICE)
+MODEL1 = nestedtensor.nn.MultiheadAttention(256, 8, dropout=0.1).to(DEVICE)
 
 def gen_t_loop_mha():
-    tensors = [torch.rand(1, i, 256).cuda() for i in RAND_INTS]
+    tensors = [torch.rand(1, i, 256).to(DEVICE) for i in RAND_INTS]
 
     def t_loop():
         for t in tensors:
-            MODEL0(t, t, t, need_weights=False)[0].sum().backward()
+            MODEL0(t, t, t, need_weights=False) #[0].sum().backward()
     return t_loop
 
 
 def gen_nt_mha():
     nt0 = nestedtensor.nested_tensor(
-        [torch.rand(i, 256) for i in RAND_INTS], device=torch.device('cuda'), dtype=torch.float)
+        [torch.rand(i, 256) for i in RAND_INTS], device=DEVICE, dtype=torch.float)
 
     def nt():
-        MODEL1(nt0, nt0, nt0, need_weights=False)[0].sum().backward()
+        MODEL1(nt0, nt0, nt0, need_weights=False) #[0].sum().backward()
     return nt
 
 
 if __name__ == "__main__":
     print(utils.benchmark_fn(gen_nt_mha()))
-    print(utils.benchmark_fn(gen_t_loop_mha()))
+    # print(utils.benchmark_fn(gen_t_loop_mha()))
--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20208314+edae05f'
-git_version = 'edae05fbeb6525526e7598acd095c1ce88331548'
+__version__ = '0.0.1.dev20208315+d1c0ecc'
+git_version = 'd1c0ecc9108c9b8de1735e8d38f2fe70264f7a57'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
