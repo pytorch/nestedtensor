af1212b6fe38b47d14620dc78efeeb2a0b63a761
cpuhrsch <cpuhrsch@fb.com>
2020-08-31 20:58:23 -0400

    im2col / col2im (#225)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20208314+edae05f'
-git_version = 'edae05fbeb6525526e7598acd095c1ce88331548'
+__version__ = '0.0.1.dev202083118+b636f39'
+git_version = 'b636f39ea12d97ea7462dda330985f1f9b572da4'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
