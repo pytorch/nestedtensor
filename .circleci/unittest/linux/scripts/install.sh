ea90e06294856c327b17a76b23978b3a42ec5fef
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-09-17 12:53:36 -0700

    Checkpoint

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -23,12 +23,6 @@ else
     cudatoolkit="cudatoolkit=${version}"
 fi
 
-# # torchvision is used for testing only
-# printf "Installing PyTorch and torchvision with %s\n" "${cudatoolkit}"
-# conda install -y -c pytorch-nightly pytorch torchvision "${cudatoolkit}"
-
-# python setup.py develop
-
 # USE_DISTRIBUTED=OFF BUILD_TEST=OFF BUILD_CAFFE2_OPS=0 USE_FBGEMM=OFF USE_NUMPY=ON USE_QNNPACK=OFF USE_PYTORCH_QNNPACK=OFF USE_CUDA=OFF USE_XNNPACK=OFF USE_NNPACK=OFF DEBUG=1 USE_NINJA=1 ./clean_build.sh
 printf "* Installing NT-specific pytorch and nestedtensor\n"
 ./clean_build.sh
