647c21ebfb2fca7470ee95c0a6852c911b8b1266
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-09-18 14:59:18 -0700

    Checkpoint

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -x
 pushd third_party/pytorch
 python setup.py develop
 popd
-pip install -v -e .
+python setup.py develop
--- clean_build.sh
+++ clean_build.sh
@@ -6,4 +6,4 @@ python setup.py clean
 python setup.py develop
 popd
 python setup.py clean
-pip install -v -e .
+python setup.py develop
