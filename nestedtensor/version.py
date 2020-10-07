03c63c0d110b13b78eb3ea6d04895511b8b7026c
cpuhrsch <cpuhrsch@fb.com>
2020-10-07 16:16:10 -0400

    Rebase pytorch submodule (#253)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202093016+681f816'
-git_version = '681f816e91588a98adcc13f167125b2e6be3f84b'
+__version__ = '0.0.1.dev202010717+c464523'
+git_version = 'c4645231b24dffd2863515e184ed7d29151399a5'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
