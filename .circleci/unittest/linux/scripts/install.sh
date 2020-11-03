4eba451b553baf505990d5f33fef14c1236511be
Nikita Shulga <nshulga@fb.com>
2020-11-03 10:08:47 -0800

    Save wheels as test job artifacts

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -26,17 +26,18 @@ conda activate ./env
 printf "Checking out submodules for pytorch build\n"
 git submodule sync
 git submodule update --init --recursive
-conda install -y numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing_extensions future six requests dataclasses hypothesis
+conda install -y numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing_extensions future six requests dataclasses hypothesis wheel
 if [ "${CU_VERSION:-}" == cpu ] ; then
     printf "* Installing NT-specific pytorch and nestedtensor cpu-only\n"
+    mkdir ~/wheels
     pushd third_party/pytorch
-    USE_DISTRIBUTED=ON BUILD_TEST=OFF USE_CUDA=OFF BUILD_CAFFE2_OPS=0 USE_NUMPY=ON USE_NINJA=1 python setup.py develop
+    USE_DISTRIBUTED=ON BUILD_TEST=OFF USE_CUDA=OFF BUILD_CAFFE2_OPS=0 USE_NUMPY=ON USE_NINJA=1 python setup.py develop bdist_wheel -b ~/wheels
     popd
     USE_NINJA=1 python setup.py develop
 else
     printf "* Installing NT-specific pytorch and nestedtensor with cuda\n"
     pushd third_party/pytorch
-    USE_DISTRIBUTED=ON BUILD_TEST=OFF USE_CUDA=ON  BUILD_CAFFE2_OPS=0 USE_NUMPY=ON USE_NINJA=1 python setup.py develop
+    USE_DISTRIBUTED=ON BUILD_TEST=OFF USE_CUDA=ON  BUILD_CAFFE2_OPS=0 USE_NUMPY=ON USE_NINJA=1 python setup.py develop bdist_wheel -b ~/wheels
     popd
     USE_NINJA=1 python setup.py develop
 fi
@@ -46,5 +47,5 @@ rm -rf /tmp/vision
 git clone https://github.com/pytorch/vision /tmp/vision
 
 pushd /tmp/vision
-python setup.py develop
+python setup.py develop bdist_wheel -b ~/wheels
 popd
