12d4be347fb47542c508a26edeeb227be6353f1b
cpuhrsch <cpuhrsch@fb.com>
2020-11-22 12:27:39 -0500

    var.dim (#283)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202011174+ce44103'
-git_version = 'ce441034e460cd18f5608e9ecf5487885ef67203'
+__version__ = '0.0.1.dev2020112216+a19c676'
+git_version = 'a19c6762b9f2b353b9289ec8f8729e10605ff563'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
