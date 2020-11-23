674ff9bd037f5fc77e70ec21736bf85bdb60f0ca
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-11-23 08:35:45 -0800

    Cleanup serialize/deserialize

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev2020112216+a19c676'
-git_version = 'a19c6762b9f2b353b9289ec8f8729e10605ff563'
+__version__ = '0.0.1.dev2020112316+12d4be3'
+git_version = '12d4be347fb47542c508a26edeeb227be6353f1b'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
