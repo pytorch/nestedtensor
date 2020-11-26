028a5a3b9196778ba4879b8121a73659f3b980a5
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos509.prn2.facebook.com>
2020-11-26 04:30:37 -0800

    2020-11-26 nightly release (6fae2bf39f50995b986fcb38206f0096394d9dee)

--- nestedtensor/csrc/autograd_functions.cpp
+++ nestedtensor/csrc/autograd_functions.cpp
@@ -42,10 +42,124 @@ Tensor NestedTensor_clone(
       src);
 }
 
+void check_dims_match_num_input_features(
+    const char* arg_name,
+    int64_t expected,
+    int64_t actual) {
+  TORCH_CHECK(
+      actual == expected,
+      arg_name,
+      " should contain ",
+      expected,
+      " elements not ",
+      actual);
+}
+
+std::vector<int64_t> make_reduce_dims(int64_t input_dim) {
+  std::vector<int64_t> result;
+  result.push_back(0);
+  for (int64_t i = 2; i < input_dim; i++) {
+    result.push_back(i);
+  }
+  return result;
+}
+
+std::vector<int64_t> make_scalar_shape(int64_t input_dim, int64_t n_input) {
+  std::vector<int64_t> result;
+  result.push_back(1);
+  result.push_back(n_input);
+  for (int64_t i = 2; i < input_dim; i++) {
+    result.push_back(1);
+  }
+  return result;
+}
+
+Tensor NestedTensor_batch_norm(
+    const Tensor& input,
+    const c10::optional<Tensor>& weight /* optional */,
+    const c10::optional<Tensor>& bias /* optional */,
+    const c10::optional<Tensor>& running_mean /* optional */,
+    const c10::optional<Tensor>& running_var /* optional */,
+    bool training,
+    double momentum,
+    double eps,
+    bool cudnn_enabled) {
+  auto num_features = input.sizes()[1];
+  if (running_mean) {
+    check_dims_match_num_input_features(
+        "running_mean", num_features, running_mean->numel());
+  } else if (!training) {
+    AT_ERROR("running_mean must be defined in evaluation mode");
+  }
+  if (running_var) {
+    check_dims_match_num_input_features(
+        "running_var", num_features, running_var->numel());
+  } else if (!training) {
+    AT_ERROR("running_var must be defined in evaluation mode");
+  }
+  if (weight) {
+    check_dims_match_num_input_features("weight", num_features, weight->numel());
+  }
+  if (bias) {
+    check_dims_match_num_input_features("bias", num_features, bias->numel());
+  }
+
+  Tensor output = at::empty_like(input, LEGACY_CONTIGUOUS_MEMORY_FORMAT);
+
+  int64_t n_input = input.size(1);
+  auto scalar_shape = make_scalar_shape(input.dim(), n_input);
+
+  at::Tensor mean;
+  at::Tensor invstd;
+  at::Tensor save_mean;
+  at::Tensor save_invstd;
+
+  if (training) {
+    auto reduce_dims = make_reduce_dims(input.dim());
+    save_mean = at::mean(input, IntArrayRef(reduce_dims));
+
+    save_invstd =
+        1 / at::sqrt(at::var(input, IntArrayRef(reduce_dims), false) + eps);
+
+    if (running_mean) {
+      at::Tensor running_mean_(running_mean->getIntrusivePtr());
+      running_mean_ = running_mean_.detach();
+      running_mean_.copy_(momentum * save_mean + (1 - momentum) * running_mean_);
+    }
+
+    if (running_var) {
+      Tensor unbiased_var = at::var(input, IntArrayRef(reduce_dims));
+      at::Tensor running_var_(running_var->getIntrusivePtr());
+      running_var_ = running_var_.detach();
+      running_var_.copy_(
+          momentum * unbiased_var + (1 - momentum) * running_var_);
+    }
+
+    mean = save_mean;
+    invstd = save_invstd;
+  } else {
+    mean = *running_mean;
+    invstd = 1 / at::sqrt(*running_var + eps);
+  }
+
+  output = input;
+  output = output - mean.reshape(IntArrayRef(scalar_shape));
+  output = output * invstd.reshape(IntArrayRef(scalar_shape));
+
+  if (weight) {
+    output = output * weight->reshape(IntArrayRef(scalar_shape));
+  }
+  if (bias) {
+    output = output + bias->reshape(IntArrayRef(scalar_shape));
+  }
+  return output;
+}
+
 TORCH_LIBRARY_IMPL(aten, AutogradNestedTensor, m) {
   // nt_impl(m, "upsample_bilinear2d", NestedTensor_upsample_bilinear2d);
   nt_impl(m, "clone", NestedTensor_clone);
   nt_impl(m, "dropout", NestedTensor_dropout);
+  nt_impl(m, "batch_norm", NestedTensor_batch_norm);
 }
 
 } // namespace at
--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev2020112422+e1b99ab'
-git_version = 'e1b99ab3a5d2bd299977b75ba03bbfdc62728ad1'
+__version__ = '0.0.1.dev202011260+ed2a1b0'
+git_version = 'ed2a1b0a2055a1f2c688d9b1418a3f072cad4713'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
