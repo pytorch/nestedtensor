fb486197b1cf116f77cc73c7f802c9bad90c1619
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos011.prn3.facebook.com>
2020-11-30 04:30:35 -0800

    2020-11-30 nightly release (d6fe30b7839fc2809559aa222f7be08dc6a2db66)

--- nestedtensor/nested/nested.py
+++ nestedtensor/nested/nested.py
@@ -360,10 +360,24 @@ class NestedTensor(metaclass=NestedTensorMeta):
         return nestedtensor.as_nested_tensor(new_tensors)
 
     def __str__(self):
-        return torch.ops.nestedtensor.str(self._impl)
+        def _str(x, indent=0, tab="  "):
+            if x.nested_dim() == 0:
+                return ""
+            s = indent*tab + "[\n"
+            if x.nested_dim() == 1:
+                strs = list(map(str, x.unbind()))
+                strs = list(map(lambda xi: "\n".join(
+                    map(lambda xij: (indent + 1)*tab + xij, xi.split("\n"))), strs))
+                s += ",\n".join(strs)
+            else:
+                s += ",\n".join(list(map(
+                    lambda xi: _str(xi, indent + 1), x.unbind())))
+            s += "\n" + indent * tab + "]"
+            return s
+        return "nested_tensor(" + _str(self) + ")"
 
     def __repr__(self):
-        return torch.ops.nestedtensor.str(self._impl)
+        return str(self)
 
     # --- impl forward ends ---
 
