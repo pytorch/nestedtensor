ae63be897b86b23a39bfdd01f79ad088467812fc
cpuhrsch <cpuhrsch@fb.com>
2020-12-01 23:22:11 -0500

    Remove dev string from package version (#304)

--- setup.py
+++ setup.py
@@ -27,10 +27,7 @@ def read(*names, **kwargs):
 
 latest_release = "0.0.1"
 
-dt = datetime.datetime.utcnow()
-package_version = "{0}.dev{1}{2}{3}{4}".format(
-    latest_release, dt.year, dt.month, dt.day, dt.hour
-)
+package_version = latest_release
 
 sha = "Unknown"
 package_name = "nestedtensor"
