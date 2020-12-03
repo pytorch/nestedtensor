99f5c1f2d9564bacc27ba64e8ea486d5d1f6db13
cpuhrsch <cpuhrsch@fb.com>
2020-12-03 00:15:56 -0500

    Change NT version string to follow torchvision conventions (#311)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev202012222+8b05de3'
-git_version = '8b05de32a0acdbfeedade06162dac5d42a73d082'
+__version__ = '0.0.1+2c3a468'
+git_version = '2c3a468de0a4a2e8c50d6dd7b41282fe98471206'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
