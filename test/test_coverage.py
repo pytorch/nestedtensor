29396ee4daeb33cfef3d04348a6b80b700773421
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-12-04 13:36:19 -0800

    Checkpoint

--- test/test_coverage.py
+++ test/test_coverage.py
@@ -23,14 +23,25 @@ class TestCoverage(TestCase):
     def test_issues_313(self):
         # Based on https://github.com/pytorch/nestedtensor/issues/313
 
-        linear = nn.Linear(9, 64)
-        norm = nn.BatchNorm1d(64)
-        # 3 voxel with 40, 50 and 90 points respectively
-        x = ntnt([torch.randn(i, 9) for i in [40, 50, 90]])
-        x = linear(x)
-        x = norm(x.transpose(2, 1).contiguous()).transpose(2, 1).contiguous()
-        x = F.relu(x)
-        x_max = torch.max(x, dim=1, keepdim=True)[0]
+        def model(x):
+            torch.manual_seed(20)
+            linear = nn.Linear(9, 64)
+            norm = nn.BatchNorm1d(64)
+            # 3 voxel with 40, 50 and 90 points respectively
+            x = linear(x)
+            x = norm(x.transpose(2, 1).contiguous()
+                     ).transpose(2, 1).contiguous()
+            x = F.relu(x)
+            return torch.max(x, dim=1, keepdim=True)[0]
+
+        inputs = [torch.randn(i, 9) for i in [40, 50, 90]]
+        model(ntnt(inputs))
+
+        inputs = [torch.randn(30, 9) for _ in range(3)]
+        x0 = model(ntnt(inputs))
+        x1 = model(torch.stack(inputs))
+        self.assertEqual(torch.stack(x0.unbind()), x1)
+
 
 if __name__ == "__main__":
     unittest.main()
