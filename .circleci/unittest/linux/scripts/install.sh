b3b4b24c38052874c6bf3bc210d09032a89b1784
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-04-06 15:55:26 -0700

    Checkpoint

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -28,10 +28,12 @@ mkdir -p $WHEELS_FOLDER
 
 PYVSHORT=${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1}
 if [ "${CU_VERSION:-}" == cpu ] ; then
+    python --version
     pip install https://download.pytorch.org/whl/nightly/cpu/torch-1.9.0.dev20210331%2Bcpu-cp${PYVSHORT}-cp${PYVSHORT}-linux_x86_64.whl
     pip install https://download.pytorch.org/whl/nightly/cpu/torchvision-0.10.0.dev20210331%2Bcpu-cp${PYVSHORT}-cp${PYVSHORT}-linux_x86_64.whl
     USE_NINJA=1 python setup.py develop bdist_wheel -d $WHEELS_FOLDER
 else
+    python --version
     pip install https://download.pytorch.org/whl/nightly/cu102/torch-1.9.0.dev20210331%2Bcpu-cp${PYVSHORT}-cp${PYVSHORT}-linux_x86_64.whl
     pip install https://download.pytorch.org/whl/nightly/cu102/torchvision-0.10.0.dev20210331%2Bcpu-cp${PYVSHORT}-cp${PYVSHORT}-linux_x86_64.whl
     USE_NINJA=1 python setup.py develop bdist_wheel -d $WHEELS_FOLDER
