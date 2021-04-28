62403cdb9599a54f570149e32d0cf17b5740ab75
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-04-28 10:20:24 -0700

    Add static inference guard

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1+aded2ac'
-git_version = 'aded2acc0344d2115e35661614192dadcfa94fe6'
+__version__ = '0.0.1+f79ec60'
+git_version = 'f79ec60c02c783defbc0b3f586eefa721496fa07'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
