c209c6def2c2a12d1011ed4a33c047d96503565f
cpuhrsch <cpuhrsch@fb.com>
2021-04-28 23:00:27 -0400

    Adopt embedding_bag to new changes (#351)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.3'
-git_version = 'b1f2ba969c1485041a45f30617270647b3cdca9d'
+__version__ = '0.1.4+5c040e0'
+git_version = '5c040e037e3131e4bc56c89c2f6a4e61a65dc282'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
