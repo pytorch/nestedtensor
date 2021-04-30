4c40faf65b72f4c1ae0de5b82c6a5c33996566ee
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-04-30 08:57:44 -0700

    Checkpoint

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-unset PYTORCH_VERSION
+# unset PYTORCH_VERSION
 # For unittest, nightly PyTorch is used as the following section,
 # so no need to set PYTORCH_VERSION.
 # In fact, keeping PYTORCH_VERSION forces us to hardcode PyTorch version in config.
