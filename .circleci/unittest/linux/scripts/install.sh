f8eea755fe5256f5405ece897e84ce1fa3a3fb40
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-05-12 18:33:31 -0700

    Checkpoint

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -51,6 +51,7 @@ if [ "${CU_VERSION:-}" == cpu ] ; then
     USE_NINJA=1 python setup.py develop bdist_wheel -d $WHEELS_FOLDER
 else
     conda install -y pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch-nightly
+    which python
     which nvcc
     nvcc --version
     FORCE_CUDA=1 USE_NINJA=1 python setup.py develop bdist_wheel -d $WHEELS_FOLDER
