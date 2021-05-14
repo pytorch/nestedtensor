bb0f72e126f8661b364977841c6bbaa6e04654aa
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-05-13 20:57:30 -0700

    Checkpoint

--- nestedtensor/csrc/storage/EfficientSizeNode.h
+++ nestedtensor/csrc/storage/EfficientSizeNode.h
@@ -17,6 +17,20 @@ namespace nested_tensor {
 //
 // } // namespace impl
 
+namespace impl {
+static at::Tensor stack_sizes(SizeNode size_node) {
+  std::vector<at::Tensor> flattened = flatten(map(
+      [](std::vector<int64_t> sizes) {
+        return torch::tensor(sizes, torch::kInt64);
+      },
+      size_node));
+  if (flattened.size() == 0) {
+    return torch::zeros({0, 0}, torch::kInt64);
+  }
+  return at::stack(flattened);
+}
+} // namespace impl
+
 struct EfficientSizeNode {
   explicit EfficientSizeNode(
       SizeNode size_node,
@@ -29,23 +43,31 @@ struct EfficientSizeNode {
               return result;
             },
             size_node))),
-        _sizes(flatten(size_node)) {}
+        _sizes(impl::stack_sizes(size_node)) {}
 
   SizeNode to_size_node() const {
-    return unflatten(deserialize_size_node(_structure), _sizes);
+    std::vector<std::vector<int64_t>> _tmp_sizes(_sizes.size(0));
+    int64_t* _sizes_ptr = _sizes.data_ptr<int64_t>();
+    for (int64_t i = 0; i < _sizes.size(0); i++) {
+      _tmp_sizes[i].resize(_sizes.size(1));
+      for (int64_t j = 0; j < _sizes.size(1); j++) {
+        _tmp_sizes[i][j] = _sizes_ptr[i * _sizes.size(1) + j];
+      }
+    }
+    return unflatten(deserialize_size_node(_structure), _tmp_sizes);
   }
   int64_t height() const {
     return _height;
   }
   int64_t dim() const {
-    return _sizes.size() > 0 ? _height + _sizes[0].size() : _height;
+    return _sizes.size(0) > 0 ? _height + _sizes.size(1) : _height;
   }
 
  private:
   int64_t _height;
   const std::vector<c10::optional<int64_t>> _opt_sizes;
   std::vector<int64_t> _structure;
-  std::vector<std::vector<int64_t>> _sizes;
+  at::Tensor _sizes;
 };
 
 } // namespace nested_tensor
--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+1adeda5'
-git_version = '1adeda5b7a3dd25c35be93de41335af585f15818'
+__version__ = '0.1.4+f1da348'
+git_version = 'f1da348b0ea026d6647fb347616f3e446c34b34f'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
