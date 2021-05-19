f5a2844b06957667110583c08208e40005f5e38a
cpuhrsch <cpuhrsch@fb.com>
2021-05-19 13:38:19 -0400

    Move more masking code into C++ (#393)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+06339d4'
-git_version = '06339d4a36d75cf78c60fd1c90fa167149304620'
+__version__ = '0.1.4+aa4359a'
+git_version = 'aa4359ac5ea5630c961631d66db67e2b902ab432'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
