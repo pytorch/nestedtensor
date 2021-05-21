4502a4832a4e476ede38e746a8039e079ab2018d
cpuhrsch <cpuhrsch@fb.com>
2021-05-21 10:40:58 -0400

    Faster to_tensor_mask (#397)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+cd74160'
-git_version = 'cd741600503ab03a967aee1a45d995dcfa7f5c3c'
+__version__ = '0.1.4+7ee35c9'
+git_version = '7ee35c9618fdb3e876b338414c22ea864be90676'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
