042462f29b9c12268ef75f0a06b41df4f8e0bd32
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared20726.06.ftw1.facebook.com>
2021-05-22 04:30:33 -0700

    2021-05-22 nightly release (4502a4832a4e476ede38e746a8039e079ab2018d)

--- nestedtensor/csrc/masking.cpp
+++ nestedtensor/csrc/masking.cpp
@@ -198,6 +198,41 @@ c10::optional<Tensor> nt_from_tensor_mask(
 std::tuple<Tensor, Tensor> to_tensor_mask(
     Tensor nt,
     c10::optional<int64_t> mask_dim) {
+#ifdef WITH_CUDA
+  if (get_dim(nt) == 3 && get_is_contiguous(nt) && mask_dim && *mask_dim == 2) {
+    auto nt_opt_size = get_opt_sizes(nt);
+    Tensor nt_buffer = get_buffer(nt);
+    if (nt_opt_size[2] && nt_buffer.is_cuda()) {
+      std::cout << "Calling efficient to_tensor_mask" << std::endl;
+      Tensor nt_sizes_ =
+          get_efficient_nested_size(nt).sizes().to(torch::kInt32);
+      TORCH_CHECK(nt_sizes_.dim() == 2, "NestedTensor must be of nested_dim 2.")
+      Tensor nt_sizes = at::native::narrow(nt_sizes_, 1, 0, 1);
+      int max_size_1 = nt_sizes.max().item<int>();
+      nt_sizes =
+          at::native::cumsum(nt_sizes, 0).to(torch::kInt32).reshape({-1});
+      nt_sizes = at::cat({torch::tensor({0}, torch::kInt32), nt_sizes});
+      Tensor output = torch::zeros(
+          {*nt_opt_size[0], max_size_1, *nt_opt_size[2]}, nt_buffer.options());
+      nt_sizes = nt_sizes.to(torch::kCUDA);
+      Tensor output_mask = torch::zeros(
+          {*nt_opt_size[0], max_size_1}, nt_buffer.options());
+      output_mask = output_mask.to(torch::kInt32);
+      at::cuda::CUDAStream defaultStream = at::cuda::getDefaultCUDAStream();
+      nested_tensor::cuda::add_padding_mask_kernelLauncher(
+          nt_buffer.data_ptr<float>(),
+          output.data_ptr<float>(),
+          output_mask.data_ptr<int>(),
+          nt_sizes.data_ptr<int>(),
+          *nt_opt_size[0],
+          output_mask.stride(0),
+          output.stride(0),
+          *nt_opt_size[2],
+          defaultStream);
+      return std::make_tuple(output, output_mask.to(torch::kBool));
+    }
+  }
+#endif
   TORCH_CHECK(
       !mask_dim || *mask_dim <= get_dim(nt),
       "Requested mask dimension ",
@@ -225,10 +260,10 @@ std::tuple<Tensor, Tensor> to_tensor_mask(
 
 Tensor to_padded_tensor(Tensor nt, double padding) {
 #ifdef WITH_CUDA
-  if (get_dim(nt) == 3) {
+  if (get_dim(nt) == 3 && get_is_contiguous(nt)) {
     auto nt_opt_size = get_opt_sizes(nt);
-    if (nt_opt_size[2]) {
-      Tensor nt_buffer = get_buffer(nt);
+    Tensor nt_buffer = get_buffer(nt);
+    if (nt_opt_size[2] && nt_buffer.is_cuda()) {
       Tensor nt_sizes_ =
           get_efficient_nested_size(nt).sizes().to(torch::kInt32);
       TORCH_CHECK(nt_sizes_.dim() == 2, "NestedTensor must be of nested_dim 2.")
