e2aaac95b3387e10d62fc5777e27ae32b4c27b07
cpuhrsch <cpuhrsch@fb.com>
2021-05-26 10:55:30 -0400

    More efficient MHA - recycle input_mask for sequence mask (#406)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+f20ca2f'
-git_version = 'f20ca2f38aaf234c1c5b85fc3b07fbe2e291cea5'
+__version__ = '0.1.4+e1d384f'
+git_version = 'e1d384fea9d70a664b38a53768f82c81057a7d13'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
