7146384c0583d0ead25746dd7ac73568381989de
Christian Puhrsch <cpuhrsch@fb.com>
2021-06-07 01:01:09 +0000

    Checkpoint

--- .circleci/unittest/linux/scripts/run_local.sh
+++ .circleci/unittest/linux/scripts/run_local.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+set -x
+set -e
 
+cd /mnt/mydata/scripts
+apt update
+apt-get -y install git wget
 export PARAMETERS_PYTHON_VERSION="3.8"
-./setup_env.sh
-./install.sh
-./run_test.sh
+.circleci/unittest/linux/scripts/setup_env.sh
+.circleci/unittest/linux/scripts/install.sh
+.circleci/unittest/linux/scripts/run_test.sh
