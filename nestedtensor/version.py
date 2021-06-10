37bb75127289d4aac113200126c1ea05dc68c74f
= <=>
2021-06-10 16:19:26 +0000

    Checkpoint

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+e11c455'
-git_version = 'e11c455b836bceff09da61ec7610ed39b72dc52b'
+__version__ = '0.1.4+dc7f190'
+git_version = 'dc7f1901bf38597385c4c2915088d3e8437d4781'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
