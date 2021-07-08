8da4a3fe499da5c9c422d7bde287149a65e57bec
cpuhrsch <cpuhrsch@fb.com>
2021-07-08 16:50:03 -0400

    Op coverage for GAT (#434)

--- nestedtensor/csrc/SoftMax.cpp
+++ nestedtensor/csrc/SoftMax.cpp
@@ -27,8 +27,27 @@ Tensor NestedTensor_softmax(
       input);
 }
 
+Tensor NestedTensor_log_softmax(
+    const Tensor& input,
+    const int64_t dim_,
+    c10::optional<ScalarType> dtype) {
+  int64_t dim = maybe_wrap_dim(dim_, get_dim(input));
+  auto input_data = get_nested_tensor_impl(input);
+  int64_t nested_dim = input_data->nested_dim();
+  TORCH_CHECK(
+      dim >= nested_dim,
+      "Cannot apply log_softmax across nested dimensions ",
+      std::to_string(dim));
+  return map_nested_tensor(
+      [dim, nested_dim, dtype](const at::Tensor t) {
+        return at::log_softmax(t, dim - nested_dim, dtype);
+      },
+      input);
+}
+
 TORCH_LIBRARY_IMPL(aten, NestedTensor, m) {
   nt_impl(m, "softmax.int", NestedTensor_softmax);
+  nt_impl(m, "log_softmax.int", NestedTensor_log_softmax);
 }
 
 } // namespace at
--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+95ac774'
-git_version = '95ac774435b94c95072f242a3ea423d538f404e3'
+__version__ = '0.1.4+33fb247'
+git_version = '33fb2477c856f8185f1e9c1e9a6ca28065e43cf9'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
