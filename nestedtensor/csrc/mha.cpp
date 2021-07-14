2bc3431ccfb1c21335cc1890523c5996ecde7fb0
= <=>
2021-07-14 23:10:51 +0000

    Checkpoint

--- nestedtensor/csrc/mha.cpp
+++ nestedtensor/csrc/mha.cpp
@@ -55,9 +55,9 @@ at::Tensor min_mha(
 
   q = q * torch::tensor(scaling);
 
-  q = q.reshape({-1, -1, num_heads, head_dim}).transpose(1, 2);
-  k = k.reshape({-1, -1, num_heads, head_dim}).transpose(1, 2);
-  v = v.reshape({-1, -1, num_heads, head_dim}).transpose(1, 2);
+  q = q.reshape({*opt_sizes[0], -1, num_heads, head_dim}).transpose(1, 2);
+  k = k.reshape({*opt_sizes[0], -1, num_heads, head_dim}).transpose(1, 2);
+  v = v.reshape({*opt_sizes[0], -1, num_heads, head_dim}).transpose(1, 2);
   auto attn_output_weights = at::matmul(q, k.transpose(2, 3));
   attn_output_weights = at::softmax(attn_output_weights, -1);
   attn_output_weights = at::dropout(attn_output_weights, dropout_p, training);
