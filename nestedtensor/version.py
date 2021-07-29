28501c30732e7ce460ab47eeecec6057ecbc6989
= <=>
2021-07-29 17:20:36 +0000

    FX based conv2d/bn and conv2d/relu fuser

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+c4a5147'
-git_version = 'c4a514795d2fa209f8df64754410b5262b3dfa45'
+__version__ = '0.1.4+eb69813'
+git_version = 'eb698135840b4664a24cfe1a47b780d553fb5626'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
