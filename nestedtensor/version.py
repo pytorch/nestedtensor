9b6dcbcba7bdd0345eea4e204b6b891576b0a2d6
Christian Puhrsch <cpuhrsch@fb.com>
2021-07-30 11:18:37 -0700

    NestedTensor import 20210730
    Summary: Code import
    
    Reviewed By: janeyx99
    
    Differential Revision: D30016452
    
    fbshipit-source-id: 211add77274159d590343e05920211b2a886b58f

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+33fb247'
-git_version = '33fb2477c856f8185f1e9c1e9a6ca28065e43cf9'
+__version__ = '0.1.4+66764fd'
+git_version = '66764fd10e9b6f9c0710840d0cb17369b9d994be'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
