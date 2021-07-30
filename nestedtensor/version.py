ee51ead76fb773f8d7214ad20e9457ac442997bd
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared15514.06.ftw1.facebook.com>
2021-07-30 04:32:48 -0700

    2021-07-30 nightly release (2818db1d83dbd475aa54aee3ab84749a4c13e911)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+c4a5147'
-git_version = 'c4a514795d2fa209f8df64754410b5262b3dfa45'
+__version__ = '0.1.4+66764fd'
+git_version = '66764fd10e9b6f9c0710840d0cb17369b9d994be'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
