f4ef7252179683b274d6c9077db9dade2d178626
= <=>
2021-08-23 23:14:40 +0000

    Updated add_relu fusion

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+da883d9'
-git_version = 'da883d94a7cb250db7ec7d6d152764e6e8e8788a'
+__version__ = '0.1.4+a3bff13'
+git_version = 'a3bff1378e04f09983668e9a210ac2b73b06c41e'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
