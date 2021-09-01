10fe5f18734072b98855836937ab48921c2666b7
Joel Benjamin Schlosser <jbschlosser@fb.com>
2021-09-01 11:07:09 -0400

    Support label_smoothing in cross_entropy

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+a3bff13'
-git_version = 'a3bff1378e04f09983668e9a210ac2b73b06c41e'
+__version__ = '0.1.4+29d58d8'
+git_version = '29d58d85ccd2c1b47fc40f2786f08c713f49acc6'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
