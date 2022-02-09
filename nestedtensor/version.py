a991f739eba015d34980027440b53c14a207b054
Christian Puhrsch <cpuhrsch@fb.com>
2022-02-09 00:43:32 +0000

    Remove unneded sync

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+6169905'
-git_version = '61699056144c04273dc0dc702253d4910ffcbd5b'
+__version__ = '0.1.4+753edd6'
+git_version = '753edd6d1823e74de1618f2672b90336d12a39ed'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
