81f953aba3902318eb8af7a9e6e1cc8fe254cda8
Christian Puhrsch <cpuhrsch@fb.com>
2022-02-09 18:14:44 +0000

    Pull in latest FT softmax

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+8d242cb'
-git_version = '8d242cb43bfbe31ef5cff11aa116c10e451aac82'
+__version__ = '0.1.4+f849e47'
+git_version = 'f849e476fa01489754fd3be8a966be2ead51d797'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
