244aecdcc9389bf6ab462248164fb62b4b018ac1
cpuhrsch <cpuhrsch@fb.com>
2022-02-11 14:32:45 -0500

    Remove PackedStorage (#464)

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+3393c9f'
-git_version = '3393c9f62b01ab048bde6bb63b094db27196c2d1'
+__version__ = '0.1.4+7dcd033'
+git_version = '7dcd03339b224161e469799fc79530650e9af137'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
