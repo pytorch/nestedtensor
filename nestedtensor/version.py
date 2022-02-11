986cfd55e2d0c8139a5e19cfca6efc740ea7ad23
Christian Puhrsch <cpuhrsch@fb.com>
2022-02-11 20:17:25 +0000

    Avoid unnecessary to device transfers

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+244aecd'
-git_version = '244aecdcc9389bf6ab462248164fb62b4b018ac1'
+__version__ = '0.1.4+93f0ada'
+git_version = '93f0ada5d53a09991dd407b0a5816367bf2a4806'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
